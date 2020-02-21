c2a8d6ff7682c15a09cecabe7d399bb188725dbe
littleRobo <littleroboo@126.com>
2020-02-22 02:51:44 +0800

    first commit

--- 3rdparty/libtiff/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/felix/Desktop/readDEM/3rdparty/tiff-4.0.10/libtiff
-# Build directory: /home/felix/Desktop/readDEM/3rdparty/tiff-4.0.10/release/libtiff
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
